d91a00c26f861a1ee174b44e437c84c730e434af
ZiqingZhao <ziqingzhao.23@gmail.com>
2024-09-26 21:09:32 +0200

    prompt fetching with dynamic inputs

--- src/generators/promptfetcher.py
+++ src/generators/promptfetcher.py
@@ -1,9 +1,11 @@
 import os
+import re
 
 
 class PromptFetcher:
-    def __init__(self, path: str = "prompts/") -> None:
+    def __init__(self, path: str = "prompts/", place_holder="[%text%]") -> None:
         self.read_prompts(path)
+        self.place_holder = place_holder
 
     def read_prompts(self, path) -> None:
         self.prompt_lib = {}
@@ -12,6 +14,10 @@ class PromptFetcher:
                 with open(f"{path}/{file}", "r") as f:
                     self.prompts_lib[os.path.splitext(file)[0]] = f.read()
 
-    def fetch(self, **kwargs) -> None:
-        for name, content in kwargs.items():
-            return self.prompts_lib[name].replace("[%text%]", content)
+    def fetch(self, prompt, *args) -> None:
+        new_prompt = self.prompt_lib[prompt]
+        for arg in args:
+            new_prompt = new_prompt.replace(
+                self.place_holder, arg, 1
+            )  # Replace only the first occurrence
+        return new_prompt
