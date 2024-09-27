8e519eae635c03bee70d0c70929d5e2e5641e638
raphael frank <04.raphael.frank@gmail.com>
2024-09-27 13:16:34 +0200

    changed app name to work as docker image

--- src/generators/ieappgenerator.py
+++ src/generators/ieappgenerator.py
@@ -14,7 +14,7 @@ class IEAppGenerator(AppGenerator):
     def __init__(
         self,
         logger: logging.Logger,
-        app_name: str = "My IE App",
+        app_name: str = "My_IE_App",
         llm_model: str = "LLaMA-3-Latest",
         api_key: str = "",
     ):
