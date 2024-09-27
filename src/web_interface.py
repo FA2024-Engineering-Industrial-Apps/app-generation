c14e2c09efbbc2759c4a8b923baf1a8408534674
Robert Hutter <spam@robert-hutter.de>
2024-09-27 21:31:53 +0200

    Added feature: empty log file at the beginning of each run.

--- src/web_interface.py
+++ src/web_interface.py
@@ -5,6 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     filename="web_interface.log",
+    filemode='w',
     encoding="utf-8",
     level=logging.DEBUG,
     format="[%(asctime)s][%(levelname)s] %(message)s",
