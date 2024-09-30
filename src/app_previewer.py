8723e96336eaef303d219fbf2aac481dba0d8196
Robert Hutter <spam@robert-hutter.de>
2024-09-30 09:39:04 +0200

    Added docstrings to all classes and methods/operations. Also deleted promptadapter.

--- src/app_previewer.py
+++ src/app_previewer.py
@@ -12,15 +12,37 @@ server_running: bool = False
 
 @server.route('/', methods=['GET'])
 def index():
+    """
+    Renders the index page for the server.
+
+    @return: The rendered 'index.html' template.
+    """
     return render_template('index.html')
 
-def ensure_empty_folder(folder_path):
-        if os.path.exists(folder_path):
-            shutil.rmtree(folder_path)
 
-        os.makedirs(folder_path, exist_ok=True)
+def ensure_empty_folder(folder_path):
+    """
+    Ensures that the specified folder is empty by removing and recreating it.
+
+    @param folder_path: The path of the folder to be cleared and recreated.
+    """
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+
+    os.makedirs(folder_path, exist_ok=True)
+    
 
 def start_preview(app: GenerationInstance) -> None:
+    """
+    Starts the application preview by serving the frontend using a local Flask server.
+
+    This method prepares the necessary files for preview by copying HTML, CSS, and JS files
+    into the preview folder and runs the server if it is not already running.
+
+    @param app: The GenerationInstance representing the application to preview.
+
+    @raise Exception: If the application architecture is not frontend-only or frontend-and-backend.
+    """
     if app.architecture not in [AppArchitecture.FRONTEND_ONLY, AppArchitecture.FRONTEND_AND_BACKEND]:
         raise Exception("Can't preview headless app.")
     
