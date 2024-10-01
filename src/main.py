3632d64368070585c42df29b7b3daed09399161a
Robert Hutter <spam@robert-hutter.de>
2024-10-01 10:35:59 +0200

    Added UI interface for deployment.

--- src/main.py
+++ src/main.py
@@ -89,7 +89,53 @@ if st.session_state['generated_app']:
             stderr=subprocess.PIPE,  # Redirect stderr
             text=True  # Decodes output as text rather than bytes
         )
-    
-    if generated_app.architecture in [AppArchitecture.FRONTEND_ONLY, AppArchitecture.FRONTEND_AND_BACKEND]:
-        if st.link_button(label='Preview App Web Interface', url='http://127.0.0.1:7654'):
-            start_preview(generated_app)
+            
+    col1, col2 = st.columns(2)
+    with col1:
+        with st.expander("publish app"):
+        # Create a form object using st.form with a specific key
+            with st.form(key='user_form'):
+                ie_user = st.text_input("IE User")
+                ie_pass = st.text_input("IE Password", type="password")
+                webaddress = st.text_input("Web Address")
+                app_name = st.text_input("App Name")
+                app_description = st.text_input("App Description")
+                icon = st.file_uploader("Upload app icon", type=['png', 'jpg', 'jpeg'])
+                version_number = st.text_input("Version Number")
+                
+                # Dropdown for category with two options: "Retail" and "Other"
+                category = st.selectbox("Category", ["Retail", "Other"])
+                
+                # Submit button for the form
+                submit_button = st.form_submit_button(label='Submit')
+                
+                # Collect all inputs into a dictionary
+                user_data = {
+                    "ie_user": ie_user,
+                    "ie_pass": ie_pass,
+                    "webaddress": webaddress,
+                    "app_name": app_name,
+                    "app_description": app_description,
+                    "icon": icon.name if icon else None,  # Handle None if no file is uploaded
+                    "version_number": version_number,
+                    "category": category
+                }
+                if submit_button:
+                # Show a success message and the user data
+                    st.success("Form submitted successfully!")
+                    if icon is not None:
+                        # Save the uploaded file to the specified path
+                        with open("publish/icon.jpg", "wb") as f:
+                            f.write(icon.read())  # Write file content to the specified path
+                    generic_parameters = {key:user_data[key] for key in ["ie_user","ie_pass","webaddress"]}
+                    app_specific_parameters = {key:user_data[key] for key in ["app_name","app_description","version_number","category"]}
+                    app_specific_parameters["project_id"] = "772dbd5041c9489e8818c849cbf5cbc0"
+                    app_specific_parameters["docker_compose_path"] = os.path.abspath(os.path.join(st.session_state['generated_app'].root_path,"docker_compose.yml")) 
+                    app_specific_parameters["app_path"]= os.path.abspath(os.path.join(st.session_state['generated_app'].root_path,"program"))
+                    publisher = Publisher(**generic_parameters)
+                    if (publisher.publish(**app_specific_parameters)):
+                        st.info("App published successfully.")
+    with col2:    
+        if st.session_state['generated_app'].architecture in [AppArchitecture.FRONTEND_ONLY, AppArchitecture.FRONTEND_AND_BACKEND]:
+            if st.link_button(label='Preview App Web Interface', url='http://127.0.0.1:7654'):
+                start_preview(st.session_state['generated_app'])
